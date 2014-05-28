89dfd3f99748e53d47182a8c44bd16ee273daf58
Vladk-el <eliott.laversin@gmail.com>
2014-05-28 20:37:08 +0200

    recuperation d'une page web ==> ok

---
+++ Projects/nurl/nurl/Nurl_tests.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Vladk
+ * Date: 14/05/2014
+ * Time: 11:51
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using NUnit.Framework;
+
+namespace nurl
+{
+	/// <summary>
+	/// Description of Nurl_tests.
+	/// </summary>
+	/// 
+	[TestFixture]
+	public class Nurl_tests
+	{
+		public Nurl_tests()
+		{
+		}
+		
+		[Test]
+		public void SayHello(){
+			var test = "test";
+			Assert.AreEqual(test.GetType(), typeof(string));
+		}
+	}
+}
---
+++ Projects/nurl/nurl/Program.cs
@@ -0,0 +1,63 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Vladk
+ * Date: 14/05/2014
+ * Time: 11:32
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Net;
+
+namespace nurl
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			
+			MainClass main = new MainClass();
+			main.test();
+			
+			
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+		
+		public void test(){
+			Console.WriteLine("Test function");
+			WebClient client = new WebClient();
+			// DownloadFile(String, String)
+			
+			DateTime before = DateTime.Now;
+			string downloadString = client.DownloadString("http://www.gooogle.com");
+			
+			DateTime after = DateTime.Now;
+			TimeSpan time = after - before;
+			
+			Console.WriteLine("Download duration : " + time.TotalMilliseconds + " (ms)");
+			
+			//client.DownloadStringCompleted += new DownloadStringCompletedEventHandler (DowloadTime);
+			
+			//Console.WriteLine(downloadString);
+		}
+		
+		public void ShowTheContentOfAPage(string url){
+			Console.WriteLine("ShowTheContentOfAPage of page : " + url);
+			WebClient client = new WebClient();
+			
+			string downloadString = client.DownloadString(url);
+			Console.WriteLine(downloadString);
+		}
+		
+		public double DowloadTime(DateTime before){
+			DateTime after = DateTime.Now;
+			
+			TimeSpan time = after - before;
+			return time.TotalMilliseconds;
+		}
+	}
+}
